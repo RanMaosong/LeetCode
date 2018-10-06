f91d42879c2ea27caf60a8f7290f1c0541dfab89
RanMaosong <maosongran@gmail.com>
2018-10-06 15:43:03 +0800

    141.环形链表

---
+++ src/main/java/com/rms/leetcode/LeetCode_141.java
@@ -0,0 +1,24 @@
+package com.rms.leetcode;
+
+/**
+ * @author Maosong Ran
+ * @date 2018/10/06
+ * @email maosongran@gmail.com
+ */
+public class LeetCode_141 {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null)
+            return false;
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != slow) {
+            if (fast == null || fast.next == null)
+                return false;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return true;
+
+    }
+}
