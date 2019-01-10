7c8325648c162c6b52a3e6cdad328551a36ed5bf
RanMaosong <maosongran@gmail.com>
2019-01-10 13:47:13 +0800

    292. Nim Game

---
+++ src/main/java/com/rms/leetcode/Leetcode_292.java
@@ -0,0 +1,12 @@
+package com.rms.leetcode;
+
+/**
+ * @author Maosong Ran
+ * @date 2019/01/10
+ * @email maosongran@gmail.com
+ */
+public class Leetcode_292 {
+    public boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
+}
