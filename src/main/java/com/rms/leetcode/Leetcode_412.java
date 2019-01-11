0a8f3eee0bf3cb438f3c591f2c24a27fac7707ef
RanMaosong <maosongran@gmail.com>
2019-01-11 17:13:38 +0800

    412. Fizz Buzz

---
+++ src/main/java/com/rms/leetcode/Leetcode_412.java
@@ -0,0 +1,28 @@
+package com.rms.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Maosong Ran
+ * @date 2019/01/11
+ * @email maosongran@gmail.com
+ */
+public class Leetcode_412 {
+    public List<String> fizzBuzz(int n) {
+        List<String> list = new ArrayList<>();
+        for (int i=1; i <= n; ++i) {
+            if ( i%3 ==0 && i%5 ==0)
+                list.add("FizzBuzz");
+            else if ( i%3 == 0)
+                list.add("Fizz");
+            else if (i%5 == 0) {
+                list.add("Buzz");
+            }
+            else
+                list.add("" + i);
+        }
+
+        return list;
+    }
+}
