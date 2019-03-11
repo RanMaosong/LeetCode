6d8b42ac40b337c9e51b2e406b9a1b2b5aabaf42
RanMaosong <maosongran@.gmail.com>
2019-03-11 16:38:14 +0800

    a

---
+++ src/main/java/com/rms/leetcode/Leetcode_883.java
@@ -0,0 +1,36 @@
+package com.rms.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Leetcode_883 {
+    public int projectionArea(int[][] grid) {
+        Map<Integer, Integer> map1 = new HashMap<>();
+        Map<Integer, Integer> map2 = new HashMap<>();
+        int res = 0;
+        int row = grid.length, col = grid[0].length;
+        for (int i=0; i < row; ++i) {
+            for (int j=0; j < col; ++j) {
+                int count = map1.getOrDefault(i, 0);
+                count = count > grid[i][j] ? count : grid[i][j];
+                map1.put(i, count);
+
+                count = map2.getOrDefault(j, 0);
+                count = count > grid[i][j] ? count : grid[i][j];
+                map2.put(j, count);
+                ++res;
+            }
+        }
+
+
+        for (int key : map1.keySet()) {
+            res += map1.get(key);
+        }
+
+        for (int key : map2.keySet()) {
+            res += map2.get(key);
+        }
+
+        return res;
+    }
+}
